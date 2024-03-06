91cd2868b83da757c39556367e32f22c6bd72354
MTK0792 <160379176+MTK0792@users.noreply.github.com>
2024-03-07 00:36:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var percentage = "90";
+if (percentage === '80') {
+    console.log("A+");
+}
+else if (percentage === '70') {
+    console.log("A");
+}
+else if (percentage === '60') {
+    console.log("B");
+}
+else if (percentage === '50') {
+    console.log("C");
+}
+else {
+    console.log("A++");
+}
